4d2c6a1b0b0b562c949433ad42cb863347f072c9
matus.hozza <matus.hozza@globallogic.com>
2017-10-24 09:32:58 +0200

    Refactor - Remove redundant getters and setters. Lombok will generate them for us.

--- gameCore/src/main/java/sk/stopangin/entity/BaseIdentifiableEntity.java
+++ gameCore/src/main/java/sk/stopangin/entity/BaseIdentifiableEntity.java
@@ -5,12 +5,4 @@ import lombok.Data;
 @Data
 public class BaseIdentifiableEntity {
     private Long id;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 }
