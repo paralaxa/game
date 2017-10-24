293bf8aca831f6f6df38f290152ca521e02dac49
jan.sedlak <jan.sedlak@globallogic.com>
2017-10-24 17:31:22 +0200

    [jan.sedlak] refactor class name

---
+++ gameCore/src/main/java/sk/stopangin/player/PlayerException.java
@@ -0,0 +1,11 @@
+package sk.stopangin.player;
+
+public class PlayerException extends RuntimeException {
+    public PlayerException(String message) {
+        super(message);
+    }
+
+    public PlayerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
