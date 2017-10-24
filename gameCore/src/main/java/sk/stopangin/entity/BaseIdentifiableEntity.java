3708bb916971bfff5d20c6a2bd852a65d3834ef0
jan.sedlak <jan.sedlak@globallogic.com>
2017-10-24 09:00:47 +0200

    [jan.sedlak] getters/setters for Player and Movement.

--- gameCore/src/main/java/sk/stopangin/entity/BaseIdentifiableEntity.java
+++ gameCore/src/main/java/sk/stopangin/entity/BaseIdentifiableEntity.java
@@ -5,4 +5,12 @@ import lombok.Data;
 @Data
 public class BaseIdentifiableEntity {
     private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- gameCore/src/main/java/sk/stopangin/movement/Movement.java
+++ gameCore/src/main/java/sk/stopangin/movement/Movement.java
@@ -7,4 +7,20 @@ import sk.stopangin.piece.Piece;
 public class Movement<T> {
     private Piece<T> piece;
     private Coordinates<T> newPocition;
+
+    public Piece<T> getPiece() {
+        return piece;
+    }
+
+    public void setPiece(final Piece<T> piece) {
+        this.piece = piece;
+    }
+
+    public Coordinates<T> getNewPocition() {
+        return newPocition;
+    }
+
+    public void setNewPocition(final Coordinates<T> newPocition) {
+        this.newPocition = newPocition;
+    }
 }
