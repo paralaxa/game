76b2dd3b7126b10a46a0f0ba36d0486e203956b8
martin.cuchran <martin.cuchran@globallogic.com>
2017-10-25 13:49:34 +0200

    .

--- gameCore/src/main/java/sk/stopangin/field/Field.java
+++ gameCore/src/main/java/sk/stopangin/field/Field.java
@@ -1,11 +1,12 @@
 package sk.stopangin.field;
 
 import lombok.Data;
+import sk.stopangin.entity.BaseIdentifiableEntity;
 import sk.stopangin.movement.Coordinates;
 import sk.stopangin.piece.Piece;
 
 @Data
-public abstract class Field<T> {
+public abstract class Field<T> extends BaseIdentifiableEntity{
     private Coordinates<T> position;
     private Piece piece;
 
