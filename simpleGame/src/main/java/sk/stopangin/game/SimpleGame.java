a77b516415548ee777cfd88d2e75fed121273d68
my <my@email.com>
2017-10-27 21:34:12 +0200

    updated generator.

--- simpleGame/src/main/java/sk/stopangin/game/SimpleGame.java
+++ simpleGame/src/main/java/sk/stopangin/game/SimpleGame.java
@@ -3,7 +3,7 @@ package sk.stopangin.game;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import sk.stopangin.board.Board;
-import sk.stopangin.board.RandomSimpleGameBoardGenerator;
+import sk.stopangin.board.ChessboardGenerator;
 import sk.stopangin.board.SimpleBoard;
 import sk.stopangin.movement.*;
 import sk.stopangin.piece.LinearMovingPiece;
@@ -33,14 +33,14 @@ public class SimpleGame extends Game<TwoDimensionalCoordinatesData, Void> {
 
     public Round<TwoDimensionalCoordinatesData, Void> startGame(List<Player<TwoDimensionalCoordinatesData>> players) {
         enrichPlayersWithPieces(players);
-        return super.startGame(RandomSimpleGameBoardGenerator.generate(), players, players.get(0));
+        return super.startGame(new SimpleBoard(ChessboardGenerator.generateFieldsForChessboard()), players, players.get(0));
     }
 
     private void enrichPlayersWithPieces(List<Player<TwoDimensionalCoordinatesData>> players) {
         long iter = 0;
         for (Player player : players) {
             player.setId(iter);
-            Piece piece = new LinearMovingPiece(player.getName() + "_piece");
+            Piece piece = new LinearMovingPiece(player.getId(), player.getName() + "_piece");
             piece.setId(player.getId());
             Set<Piece> pieces1 = new HashSet<>();
             pieces1.add(piece);
