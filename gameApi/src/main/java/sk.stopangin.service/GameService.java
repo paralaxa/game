f719f7b6da5688d6e86724726c5dfaad5cc07d18
my <my@email.com>
2017-10-29 19:37:03 +0100

    refactor and api unification

--- gameApi/src/main/java/sk.stopangin.service/GameService.java
+++ gameApi/src/main/java/sk.stopangin.service/GameService.java
@@ -1,16 +1,25 @@
 package sk.stopangin.service;
 
 import sk.stopangin.board.Board;
+import sk.stopangin.field.Action;
 import sk.stopangin.game.Round;
 
 import java.io.Serializable;
 
-public interface GameService<T extends Serializable, R> {
+public interface GameService<T extends Serializable, R, A extends Serializable> {
 
 
     Long startGame(String playerName);
 
-    Round<T, R> commitRound(Long gameId,  T coordinatesData);
+
+    Round<T, R> commitRound(Long gameId, T coordinatesData);
+
 
     Board<T> getBoard(Long gameId);
+
+
+    A performAction(Long gameId, String data);
+
+
+    Action<A, T, R> getActionForCurrentRound( Long gameId);
 }
--- simpleGame/src/main/java/sk/stopangin/service/impl/GameServiceImpl.java
+++ simpleGame/src/main/java/sk/stopangin/service/impl/GameServiceImpl.java
@@ -6,12 +6,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import sk.stopangin.board.Board;
 import sk.stopangin.board.SimpleBoard;
+import sk.stopangin.field.Action;
+import sk.stopangin.field.ActionField;
 import sk.stopangin.field.Field;
 import sk.stopangin.field.Question;
 import sk.stopangin.field.util.SimpleGameFieldsGenerator;
 import sk.stopangin.game.Game;
 import sk.stopangin.game.Round;
 import sk.stopangin.game.SimpleGame;
+import sk.stopangin.movement.Coordinates;
 import sk.stopangin.movement.Movement;
 import sk.stopangin.movement.TwoDimensionalCoordinates;
 import sk.stopangin.movement.TwoDimensionalCoordinatesData;
@@ -21,14 +24,14 @@ import sk.stopangin.repository.GameRepository;
 import sk.stopangin.repository.QuestionsRepository;
 import sk.stopangin.service.GameService;
 
-import javax.validation.constraints.NotNull;
+import javax.ws.rs.QueryParam;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 @RestController
 @RequestMapping("game")
-public class GameServiceImpl implements GameService<TwoDimensionalCoordinatesData, Void> {
+public class GameServiceImpl implements GameService<TwoDimensionalCoordinatesData, Void, Integer> {
     private static final Logger log = LoggerFactory.getLogger(GameServiceImpl.class);
 
     @Autowired
@@ -51,7 +54,7 @@ public class GameServiceImpl implements GameService<TwoDimensionalCoordinatesDat
     }
 
     @PostMapping(path = "/{gameId}/commit", consumes = "application/json", produces = "application/json")
-    public Round<TwoDimensionalCoordinatesData, Void> commitRound(@PathVariable(value = "gameId", required = true)  Long gameId, @RequestBody(required = true)TwoDimensionalCoordinatesData twoDimensionalCoordinatesData) {
+    public Round<TwoDimensionalCoordinatesData, Void> commitRound(@PathVariable(value = "gameId", required = true)  Long gameId, @RequestBody(required = true) TwoDimensionalCoordinatesData twoDimensionalCoordinatesData) {
         log.info("committing round for game:{} with coordinates data:{}", gameId, twoDimensionalCoordinatesData);
         Game<TwoDimensionalCoordinatesData, Void> game = gameRepository.getById(gameId);
         Movement<TwoDimensionalCoordinatesData> movement = new Movement<>();
@@ -59,9 +62,27 @@ public class GameServiceImpl implements GameService<TwoDimensionalCoordinatesDat
         return game.commitRound(movement);
     }
 
-
     @GetMapping(path = "/{gameId}/board", produces = "application/json")
-    public Board<TwoDimensionalCoordinatesData> getBoard(@PathVariable("gameId") @NotNull Long gameId) {
+    public Board<TwoDimensionalCoordinatesData> getBoard(@PathVariable(value = "gameId", required = true) Long gameId) {
         return gameRepository.getById(gameId).getBoard();
+    }
+
+    @GetMapping(path = "/{gameId}/action/perform")
+    public Integer performAction(@PathVariable(value = "gameId", required = true) Long gameId, @QueryParam("data") String data) {
+        Game<TwoDimensionalCoordinatesData, Void> game = gameRepository.getById(gameId);
+        Action<Integer, TwoDimensionalCoordinatesData, Void> action = getActionForCurrentRound(gameId);
+        return action.perform(game, data);
+    }
+
+    @GetMapping(path = "/{gameId}/action")
+    public Action<Integer, TwoDimensionalCoordinatesData, Void> getActionForCurrentRound(@PathVariable(value = "gameId", required = true)Long gameId) {
+        Game<TwoDimensionalCoordinatesData, Void> game = gameRepository.getById(gameId);
+        Movement<TwoDimensionalCoordinatesData> movement = game.getActiveRound().getActualPossition();
+        Coordinates<TwoDimensionalCoordinatesData> currentRoundPosition = movement.getNewPosition();
+        Field<TwoDimensionalCoordinatesData> fieldForCoordinates = game.getBoard().getFieldForCoordinates(currentRoundPosition);
+        if (fieldForCoordinates instanceof ActionField) {
+            return ((ActionField<Integer, TwoDimensionalCoordinatesData, Void>) fieldForCoordinates).getAction();
+        }
+        return null;
     }
 }
